224e47c4cd7585c6df0cadbb66e19c04b879c797
giagra91 <giagra91@gmail.com>
2022-03-18 15:28:54 +0100

    completamento JS

--- js/script.js
+++ js/script.js
@@ -2,11 +2,61 @@ let app = new Vue(
     {
         el: `#app`,
         data: {
-            check : true,
+            todoListItems : [
+                {
+                    text : `Fare la spesa`,
+                    done : true,
+                },
+                {
+                    text : `Passare in lavanderia`,
+                    done : false,
+                },
+                {
+                    text : `Lavare la macchina`,
+                    done : true,
+                },
+                {
+                    text : `Portare a spasso il gatto`,
+                    done : false,
+                },
+                {
+                    text : `Preparare il pranzo`,
+                    done : false,
+                },
+            ],
+            newText : "",
+            newDone : "",
+            // myColor : ""
         },
         methods : {
-            
+            addItem(){
+                if(this.newDone == 1){
+                    this.newDone = true;
+                } else {
+                    this.newDone = false;
+                }
+
+                if(this.newText !== "" && this.newDone || !this.newDone){
+                    this.todoListItems.push(
+                        {
+                            text : this.newText,
+                            done : this.newDone,
+                        },
+                    )
+                    this.newDone = "",
+                    this.newText = ""
+                } else {
+                    console.warn(`Devi aggiungere un elemento alla to do list! Non fare il furbo con me!`)
+                }
+                // console.error(this.myColor)
+            },
+            removeItem(index, array){
+                console.log(index);
+                array.splice(index, 1)
+            },
+            invertDone(index){
+                this.todoListItems[index].done = !this.todoListItems[index].done;
+            }
         }
     }
-
 )
